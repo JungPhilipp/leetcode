0712dfc89844c024f47b3621e43f6f3e4024232c
Philipp Jung <philipp.jung@mailbox.org>
2022-04-14 11:03:12 +0200

    Problem1: Two sums, simple solution

--- src/prob_1_two_sums/test_two_sums.py
+++ src/prob_1_two_sums/test_two_sums.py
@@ -1,2 +1,31 @@
-def test_increment():
-    assert 4 == 4
+from .two_sums import Solution
+
+solution = Solution()
+
+
+def test_empty():
+    assert solution.twoSum([], 10) == []
+
+
+def test_example1():
+    assert solution.twoSum([2, 7, 11, 15], 9) == [0, 1]
+
+
+def test_example2():
+    assert solution.twoSum([3, 2, 4], 6) == [1, 2]
+
+
+def test_example3():
+    assert solution.twoSum([3, 3], 6) == [0, 1]
+
+
+def test_do_no_reuse_element():
+    assert solution.twoSum([3], 6) == []
+
+
+def test_negativ_numbers():
+    assert solution.twoSum([-3, -2, -1, 0, -40], -4) == [0, 2]
+
+
+def test_negativ_positive_numbers_mixed():
+    assert solution.twoSum([-3, 2, -2, 0, 40], 0) == [1, 2]
--- src/prob_1_two_sums/two_sums.py
+++ src/prob_1_two_sums/two_sums.py
@@ -2,5 +2,13 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pass
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        if len(numbers) == 0:
+            return []
+        for first_index, first in enumerate(numbers):
+            for second_index, second in enumerate(numbers):
+                if first_index == second_index:
+                    continue
+                if first + second == target:
+                    return [first_index, second_index]
+        return []
