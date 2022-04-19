6d082e4f9a9a661fdfc116e177f75878c2db5bc5
Philipp Jung <philipp.jung@mailbox.org>
2022-04-19 12:07:58 +0200

    Problem9: Palindrom number, without string conversion

--- src/prob_9_palindrome_number/palindrome_number.py
+++ src/prob_9_palindrome_number/palindrome_number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isPalindrome(self, num: int) -> bool:
+    def isPalindromeSimple(self, num: int) -> bool:
         if num < 0:
             return False
         num_string = str(num)
@@ -12,3 +12,22 @@ class Solution:
             front += 1
             back -= 1
         return True
+
+    def isPalindrome(self, num: int) -> bool:
+        if num < 0 or (num > 0 and num % 10 == 0):
+            return False
+
+        reversed_num = 0
+        while num > reversed_num:
+            reversed_num = reversed_num * 10 + num % 10
+            num //= 10
+        return bool(num == reversed_num or num == reversed_num // 10)
+
+
+# Simple Solution:
+# O(n) for int to string conversion + O(n) to check all digits -> Overall O(n)
+# Cannot improve asymptotic time complexity because all digits need to be checked to be sure
+# O(n) memory, because of copied string (n being the number of digits)
+
+# Improved Solution:
+# Use mod to access digits instead of string conversion.
