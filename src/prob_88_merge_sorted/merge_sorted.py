7f7b4a20659958cd2857bfe4fe5068ce45a5373a
Philipp Jung <philipp.jung@mailbox.org>
2024-01-11 09:23:56 +0100

    Add optimized solution for problem 88

--- src/prob_88_merge_sorted/merge_sorted.py
+++ src/prob_88_merge_sorted/merge_sorted.py
@@ -1,6 +1,21 @@
-from typing import List
+import sys
+from typing import List, Optional
+
 
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        nums1[m:m + n+1] = nums2
-        nums1.sort()
+        out = nums1
+        out_index = m + n - 1
+        in_index1 = m - 1
+        in_index2 = n - 1
+        while out_index >= 0:
+            in1 = nums1[in_index1] if in_index1 >= 0 else -1 * sys.maxsize
+            in2 = nums2[in_index2] if in_index2 >= 0 else -1 * sys.maxsize
+
+            if in1 > in2:
+                out[out_index] = in1
+                in_index1 -= 1
+            else:
+                out[out_index] = in2
+                in_index2 -= 1
+            out_index -= 1
