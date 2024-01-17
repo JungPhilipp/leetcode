9a0af23866f8ea50f15a94493ba64632021a1d28
Philipp Jung <philipp.jung@mailbox.org>
2024-01-17 15:42:01 +0100

    Add optimized solution for problem 121

--- src/prob_121_best_time/best_time.py
+++ src/prob_121_best_time/best_time.py
@@ -1,15 +1,25 @@
 from typing import List
 
+import sys
+
 # Complexity simple:
 #   * Runtime: O(n^2) Check all pairs
 #   * Space: O(1)
+# Complexity best conceivable:
+#   * Runtime: O(n) look at every element only once
+#   * Space: O(1) only two variables
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         max_profit = 0
-        for (index, buy_price) in enumerate(prices):
-            for sell_price in prices[index:]:
-                profit = sell_price - buy_price
-                if profit > max_profit:
-                    max_profit = profit
+        min_price = sys.maxsize
+        for price in prices:
+            if price < min_price:
+                min_price = price
+                continue
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+
         return max_profit
