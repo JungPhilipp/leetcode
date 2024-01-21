d39b2070da4ddc82bc2973ea4e998779a8269fa0
Philipp Jung <philipp.jung@mailbox.org>
2024-01-21 12:31:41 +0100

    Add simple optimized for problem 28

--- src/prob_28_first_occurrence/first_occurrence.py
+++ src/prob_28_first_occurrence/first_occurrence.py
@@ -1,16 +1,29 @@
-
-
 # Complexity simple:
 #   * Runtime: O(n^2) For each start position check if needle is next word (could be n log)
 #   * Space: O(1)
 # Complexity best conceivable:
 #   * Runtime: O(n) look at every element only once
-#   * Space: O(1) only constant number of variables
+#   * Space: O(n) only constant number of variables
 
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for index, _ in enumerate(haystack):
-            if haystack[index:index+len(needle)] == needle:
-                return index
+        stack = []
+        for index, c in enumerate(haystack):
+            stack2 = []
+            for start in stack:
+                sub_index = index - start
+                if sub_index == len(needle):
+                    return start
+                if c == needle[sub_index]:
+                    stack2.append(start)
+            if c == needle[0]:
+                stack2.append((index))
+            stack[:] = stack2
+
+        if len(stack) == 0:
+            return -1
+        for start in stack:
+            if len(haystack) - start == len(needle):
+                return start
         return -1
--- src/prob_28_first_occurrence/test_first_occurrence.py
+++ src/prob_28_first_occurrence/test_first_occurrence.py
@@ -4,8 +4,14 @@ solution = Solution()
 
 
 def test_example_1():
-    assert solution.strStr("sadbutsad", "sad") == 0
+    assert solution.strStr("ssadbutsad", "sad") == 1
 
 
 def test_example_2():
     assert solution.strStr("leetcode", "leeto") == -1
+
+def test_example_3():
+    assert solution.strStr("a", "a") == 0
+
+def test_example_4():
+    assert solution.strStr("aaa", "aaaa") == -1
