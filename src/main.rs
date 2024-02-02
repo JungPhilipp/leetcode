691320311420c53bcd23eaf047c6ba540abee83d
Philipp Jung <philipp.jung@mailbox.org>
2024-02-02 09:41:42 +0100

    Add simple solution for prob 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+mod prob_1_two_sums;
 mod prob_14_longest_common_prefix;
 mod prob_27_remove_element;
 
---
+++ src/prob_1_two_sums/mod.rs
@@ -0,0 +1,54 @@
+use std::vec::Vec;
+struct Solution {}
+
+/// * Time Complexity: O(n2)
+/// * Space Complexity: O(1)
+fn two_sum_simple(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for (i, first) in nums.iter().enumerate() {
+        for (j, second) in nums[i + 1..].iter().enumerate() {
+            if first + second == target {
+                return vec![i as i32, (i + 1 + j) as i32];
+            }
+        }
+    }
+
+    vec![]
+}
+
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        two_sum_simple(nums, target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Solution;
+
+    #[test]
+    fn test_example_1() {
+        assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), [0, 1]);
+    }
+
+    #[test]
+    fn test_example_2() {
+        assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), [1, 2]);
+    }
+
+    #[test]
+    fn test_example_3() {
+        assert_eq!(Solution::two_sum(vec![3, 3], 6), [0, 1]);
+    }
+
+    #[test]
+    fn test_empty() {
+        assert_eq!(Solution::two_sum(vec![], 2), []);
+    }
+
+    #[test]
+    fn test_target_not_possible() {
+        assert_eq!(Solution::two_sum(vec![2, 16, 4, 2], 100), []);
+        assert_eq!(Solution::two_sum(vec![2, 16, 4, 1], 199), []);
+    }
+}
