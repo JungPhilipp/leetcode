2997fface2915f94f74340f5c9fe8791b864021d
Philipp Jung <philipp.jung@mailbox.org>
2024-02-05 09:35:47 +0100

    Add iterative solution for prob 104

--- src/prob_104_max_depth_bin_tree/max_depth_bin_tree.py
+++ src/prob_104_max_depth_bin_tree/max_depth_bin_tree.py
@@ -11,8 +11,29 @@ class TreeNode:
 
 #   * Runtime: O(n): iterate through all nodes once
 #   * Space: O(log n): stack space for recursion
-class Solution:
+class SolutionRecursive:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+#   * Runtime: O(n): iterate through all nodes once
+#   * Space: O(log n): stack to keep track of nodes to be visited
+class SolutionIterative:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        stack = [(root, 0)]
+        max = 0
+        while len(stack) != 0:
+            (next, depth) = stack.pop()
+            if not next:
+                if depth > max:
+                    max = depth
+                continue
+
+            stack.append((next.left, depth + 1))
+            stack.append((next.right, depth + 1))
+        return max
+
+
+Solution = SolutionIterative
