6c693af879d0ae98aa8c9c29667a5c92fbbc132a
Philipp Jung <philipp.jung@mailbox.org>
2024-02-14 10:42:14 +0100

    Add solution for prob 112

---
+++ src/prob_112_path_sum/path_sum.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+#   * Runtime: O(n): visit all nodes
+#   * Space: O(log n): stack space for recursion
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        targetSum -= root.val
+        if not root.left and not root.right:
+            return targetSum == 0
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(
+            root.right, targetSum
+        )
